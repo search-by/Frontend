9d5609a426457f86be3f23e22dc616ec09b0a27e
drevilphd <aleksandr.yur.p@gmail.com>
2022-11-21 14:54:38 +0200

    Task.Serializer add lot of params

--- fe_bot/tasks/serializers.py
+++ fe_bot/tasks/serializers.py
@@ -8,6 +8,11 @@ class TaskSerializer(serializers.Serializer):
     source_adres = serializers.CharField()
     chat_id = serializers.IntegerField()
     PY_max_results = serializers.IntegerField()
+    PY_tolerance = serializers.DecimalField(max_digits=5, decimal_places=3)
+    FC_max_results = serializers.IntegerField()
+    FC_tolerance = serializers.DecimalField(max_digits=5, decimal_places=3)
+    send_full_results = serializers.BooleanField()
+    send_fedback_request = serializers.BooleanField()
 
     def update(self, instance, validated_data):
         instance.status = validated_data.get("status", instance.status)
@@ -16,4 +21,8 @@ class TaskSerializer(serializers.Serializer):
 
     class Meta:
         model = Task
-        fields = ("source_adres", "chat_id", "status", "PY_max_results")
+        fields = ("source_adres", "chat_id", "status", "PY_max_results", "PY_tolerance",
+                  "FC_max_results",
+                  "FC_tolerance",
+                  "send_full_results",
+                  "send_fedback_request")
